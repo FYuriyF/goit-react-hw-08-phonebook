387929b112ba76cba17959ee472969f96a2d61b3
FYuriyF <115367798+FYuriyF@users.noreply.github.com>
2023-06-30 19:06:58 +0000

    Deploying to gh-pages from @ FYuriyF/goit-react-hw-08-phonebook@efba071038a8a3889e3bc02e15a2ef3a6c7911da 🚀

---
+++ static/js/936.066228b5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[936],{936:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r="Home_title__L087v",c=n(184);function o(){return(0,c.jsx)("div",{children:(0,c.jsx)("h1",{className:r,children:"Welcome to the final version of contact storage"})})}}}]);
+//# sourceMappingURL=936.066228b5.chunk.js.map
